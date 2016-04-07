d931a5e8cfbf8e3f8ea36e85dbc6dbd5e3a7ced0
Tony <toninus.lcm@gmail.com>
2016-04-07 17:55:30 +0200

    fatto javabean

--- src/it/csttech/etltools/CsvFileExtractor.java
+++ src/it/csttech/etltools/CsvFileExtractor.java
@@ -6,7 +6,28 @@ import java.util.*;
  * PlaceHolder
  */
 public class CsvFileExtractor implements Extractor {
+	
+   private String fileName;
 
+	/*
+	 * Transform the readed line to a record
+	 */
+	protected static List<Record> parseLine (String inputLine){
+		String[] parsedLine inputLine.split(	"\";\""	+ "|" + 
+								"\";"	+ "|" + 
+								";\""	+ "|" + 
+								";"	+ "|" + 
+								"\"");
+		Record output = new Record();
+		
+		
+	}
+
+   
+   public CsvFileExtractor( String fileName){
+	   this.fileName = fileName ;
+   }
+	
   /*
    * PlaceHolder 
    * 
--- src/it/csttech/etltools/Record.java
+++ src/it/csttech/etltools/Record.java
@@ -2,12 +2,48 @@ package it.csttech.etltools;
 
 import java.util.Date;
 
-public class Record{
+public class Record implements java.io.Serializable {
 
 	private int id;
 	private String name;
 	private Date birthday;
 	private double height;
-	private boolean marriedStatus;
+	private boolean married;
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return this.birthday;
+    }
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+   public int getHeight() {
+        return this.height;
+    }
+    public void setHeight(int id) {
+        this.height = height;
+    }
+
+    // Diversa sintassi per gli attributi boolean ( 'is' al posto di 'get' )
+    public boolean isMarried() {
+        return this.married;
+    }
+    public void setMarried(boolean married) {
+        this.married = married;
+
 
 }
