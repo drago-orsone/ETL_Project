8c024dceebb3f477cbb46be7c20a21ce3b208d5e
Tony <toninus.lcm@gmail.com>
2016-04-26 16:50:48 +0200

    Extractor factory with Reflection!

--- src/it/csttech/etltools/extractor/ExtractorFactory.java
+++ src/it/csttech/etltools/extractor/ExtractorFactory.java
@@ -6,12 +6,17 @@ import it.csttech.etltools.ETLException;
 import java.lang.reflect.Constructor;
 
 /**
- *
- */
+*
+*/
 public class ExtractorFactory {
 
 	private Properties properties;
 
+	/**
+	 * [ExtractorFactory description]
+	 * @param  properties [description]
+	 * @return            [description]
+	 */
 	public ExtractorFactory(Properties properties){
 		this.properties = properties;
 	}
@@ -22,24 +27,16 @@ public class ExtractorFactory {
 	* @return               [description]
 	*/
 	public Extractor getExtractor(String extractorType) throws ETLException {
+		String extractorClassName = properties.getProperty(extractorType + ".extractor.class".toLowerCase());
+		if(extractorClassName == null )
+			throw new ETLException("Invalid input format " + extractorType + ".");
 		try{
-			String extractorClassName = properties.getProperty(extractorType + ".extractor.class".toLowerCase());
 			Constructor ctor = Class.forName(extractorClassName).getDeclaredConstructor(Properties.class);
 			return (Extractor)ctor.newInstance(properties);
 		} catch (ReflectiveOperationException ex) {
-			ex.printStackTrace();
-			throw new ETLException("Invalid input format " + extractorType + ".");
+			throw new ETLException("Cannot find : " + ex.getMessage() + "class" );
+				//i possibili errori sono 3: non trovo la classe, la classe non ha tale costruttore, la classe non può essere istanziata!
 		}
-
-		/*
-		switch (extractorType.toLowerCase()) {
-			case "csv": return new CsvFileExtractor(properties);
-			case "fw": return new FwFileExtractor(properties);
-			case "xml": return new XmlFileExtractor(properties);
-			case "db": return new SqliteExtractor(properties);
-			case "sys": return new SystemExtractor(properties);
-			default: throw new ETLException("Invalid input format " + extractorType + ".");
-
-		} */
 	}
+
 }
